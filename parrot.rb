830c0dfbe73efe8c1c7bf0c0f2fd5ca1e3b4cf42
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-06-15 17:49:45 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(input="Squawk!")
-  if (input == ""){
-    puts "Squawk!"
-  }
-  else{
+  if input == ""
+    puts `Squawk!`
+  else
     puts "#{input}"
-  }
   end
 end
