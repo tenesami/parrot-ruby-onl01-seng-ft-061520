3e2c3bbb7219d946483a150fd7955f87486caec9
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-06-15 17:12:11 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(input)
-   puts "Squawk! #{input}"
+def parrot(input="Squawk!")
+   puts  "#{input}"
 end
 
 parrot();
